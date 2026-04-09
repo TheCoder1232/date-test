0b17da8bbbe1d9cb12cf3445ace29a8b8ccfcbb4
Aditya Fukate <aditya_sanjayfukate@epam.com>
2026-04-09 17:50:16 +0530

    added main file

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+if __name__ == "__main__:
+    print(os.__version__)
+    print("Hello World!!!")
+    print("Hi there new time")
+    print("new line")    
+    print("rock this shit")
+    
